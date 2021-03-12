d97080b101cca3d8b6dc53cf75557ace6f86fece
Adib Wirawan P <adib.w.p@mail.ugm.ac.id>
2021-03-13 06:24:48 +0700

    progres part 1

---
+++ app/src/main/java/com/haerul/foodsapp/view/detail/DetailPresenter.java
@@ -0,0 +1,53 @@
+/*-----------------------------------------------------------------------------
+ - Developed by Haerul Muttaqin                                               -
+ - Last modified 4/7/19 7:07 PM                                               -
+ - Subscribe : https://www.youtube.com/haerulmuttaqin                         -
+ - Copyright (c) 2019. All rights reserved                                    -
+ -----------------------------------------------------------------------------*/
+package com.haerul.foodsapp.view.detail;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.haerul.foodsapp.Utils;
+import com.haerul.foodsapp.model.Meals;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class DetailPresenter {
+    private DetailView view;
+
+    public DetailPresenter(DetailView view) {
+        this.view = view;
+    }
+
+    void getMealById(String mealName) {
+        
+        //TODO #5 Call the void show loading before starting to make a request to the server (DONE)
+        view.showLoading();
+        
+        //TODO #6 Make a request to the server (Don't forget to hide loading when the response is received) (DONE)
+        Call<Meals> mealsCall = Utils.getApi().getMealByName(mealName);
+        mealsCall.enqueue(new Callback<Meals>() {
+            @Override
+            public void onResponse(@NonNull Call<Meals> call,@NonNull Response<Meals> response) {
+                view.hideLoading();
+                if (response.isSuccessful() && response.body() != null) {
+                    view.setMeals(response.body().getMeals());
+                } else {
+                    view.onErrorLoading(response.message());
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Meals> call,@NonNull Throwable t) {
+                view.hideLoading();
+                view.onErrorLoading(t.getLocalizedMessage());
+            }
+        });
+        
+        //TODO #7 Set response (meal) (DONE)
+    }
+}

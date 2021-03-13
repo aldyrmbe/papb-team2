62a749460c74a6e1e5ef3e7a3f5544dad0f0c993
ryanakhra2 <fajar.ryan.akhra@mail.ugm.ac.id>
2021-03-13 23:13:10 +0700

    perbaiki package dan gradle

---
+++ app/src/main/java/com/team2/foodrecipes/view/detail/DetailPresenter.java
@@ -0,0 +1,52 @@
+/*-----------------------------------------------------------------------------
+ - Developed by Haerul Muttaqin                                               -
+ - Last modified 4/7/19 7:07 PM                                               -
+ - Subscribe : https://www.youtube.com/haerulmuttaqin                         -
+ - Copyright (c) 2019. All rights reserved                                    -
+ -----------------------------------------------------------------------------*/
+package com.team2.foodrecipes.view.detail;
+
+import androidx.annotation.NonNull;
+
+import com.team2.foodrecipes.Utils;
+import com.team2.foodrecipes.model.Meals;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class DetailPresenter {
+    private DetailView view;
+
+    public DetailPresenter(DetailView view) {
+        this.view = view;
+    }
+
+    void getMealById(String mealName) {
+        
+        //TODO #5 Call the void show loading before starting to make a request to the server (DONE)
+        view.showLoading();
+        
+        //TODO #6 Make a request to the server (Don't forget to hide loading when the response is received) (DONE)
+        Utils.getApi().getMealByName(mealName)
+                .enqueue(new Callback<Meals>() {
+                    @Override
+                    public void onResponse(@NonNull Call<Meals> call,@NonNull Response<Meals> response) {
+                        view.hideLoading();
+                        if (response.isSuccessful() && response.body() != null) {
+                            view.setMeals(response.body().getMeals().get(0));
+                        } else {
+                            view.onErrorLoading(response.message());
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<Meals> call,@NonNull Throwable t) {
+                        view.hideLoading();
+                        view.onErrorLoading(t.getLocalizedMessage());
+
+                    }
+                });
+        //TODO #7 Set response (meal)
+    }
+}
